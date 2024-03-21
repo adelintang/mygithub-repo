63b81779a894253cc9adbfbfd91324d56d16da24
adelintang <adelintang157@gmail.com>
2024-03-21 09:39:03 +0700

    feat(page): about

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+const AboutPage = async () => {
+  return (
+    <>
+      <h2 className="text-lg font-medium mb-4">About</h2>
+      <p className="text-slate-700 mb-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem iste ullam mollitia obcaecati aperiam eos deleniti impedit eveniet? Cumque quaerat quibusdam ullam ex dicta eius maxime repudiandae consectetur? Saepe veritatis quis atque velit nobis dolores assumenda nam praesentium impedit cupiditate iusto quo nostrum repudiandae laborum voluptatum esse, quasi ab consequuntur.</p>
+      <p className="text-slate-700">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam provident voluptas ad id autem aliquam libero laudantium numquam ipsum inventore, perferendis molestias ex, exercitationem repellendus unde veniam facere quibusdam! Culpa.</p>
+    </>
+  )
+}
+
+export default AboutPage
